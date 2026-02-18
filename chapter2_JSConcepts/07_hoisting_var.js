6c424ea51512da01b865887c4ae92c96c53404f6
Mansideshman <mdeshman0429@gmail.com>
2026-02-18 17:06:03 +0530

    Add new JS files for hoisting examples in chapter2

---
+++ chapter2_JSConcepts/07_hoisting_var.js
@@ -0,0 +1,22 @@
+// //var a = undefined;
+// console.log(a);
+// var a = "Mansi";
+
+//var a ="Mansi Deshman";
+
+// console.log(a);
+// var a = "mansi";
+// console.log(b);
+// var b = "deshman";
+
+
+//console.log("greeting");
+/*var greeting = "Hello";
+console.log(greeting);
+*/
+
+console.log(a);
+console.log("asscavff ");
+console.log("fvfvvss");
+console.log("dscsvvfevfe");
+var a="Mansi";
---
+++ chapter2_JSConcepts/08_hoisting_let.js
@@ -0,0 +1,17 @@
+// {
+// //console.log(score);
+// //score = 50;
+//  //typeofscore = "number";
+// let score =50;
+// console.log(score);
+// }
+
+
+//let x= global;
+if(true)
+{
+    //console.log(x);
+  //  let x = "block";
+    console.log(x);
+    let x = "block";
+}
