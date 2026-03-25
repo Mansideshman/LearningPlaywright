bc89abb16ca6ee8df8028e08c96a7309488a75a0
Mansideshman <mdeshman0429@gmail.com>
2026-03-25 15:39:46 +0530

    Strings_task updated

--- chapter10_Strings/task_strings.js
+++ chapter10_Strings/task_strings.js
@@ -2,29 +2,52 @@
 
 //let url = "https://staging.myapp.com/dashboard";
 
-//console.log(url.includes("staging"));
+//console.log(url.includes("staging"));   // true
 
-// console.log(url.startsWith("https"));
+// console.log(url.startsWith("https")); //true
 
-// console.log(url.endsWith("/dashboard"));
+// console.log(url.endsWith("/dashboard")); //true
 
+//--------------------------------------------------------------------------
 
 // let log = "[ERROR] 2024-03-07 TestCase: login - Status: 500";
-// let status = log.match(/Status: (\d+)/)[1];      //500
+// let status = log.match(/Status: (\d+)/)[1];  //extracts status code from log
+// console.log(status)  ;  //500
+//-------------------------------------------------------------------------------
+//masking sensitive information
 
 let env = "staging"; 
-let module = "auth"; 
+let setup = "auth"; 
 let count = 7; 
-let testId = `${env}_${module}_${String(count).padStart(3, "0")}`;
+let testId = `${env}_${setup}_${String(count).padStart(3,"0")}`;
+console.log(testId);   // staging auth_007
 
-//let actual = " PASS "; let expected = "pass"; actual.trim().toLowerCase() === expected;
-
-
-
-// let testUrl = "https://app.com/search?query=login&page=2&sort=asc"; let params = Object.fromEntries( testUrl.split("?")[1].split("&").map(p => p.split("=")) );
+//-------------------------------------------------------------------------------------
 
 
-// —- 
+//// ✅ Normalizing test results
+
+// let actual = " PASS ";
+//  let expected = "pass";
+  
+// console.log(actual.trim().toLowerCase()=== expected);   // true
+
+
+// //--------------------------------------------------------------------------------
+
+
+
+
+
+// // ✅ Parsing query parameters from URL
+
+// let testUrl = "https://app.com/search?query=login&page=2&sort=asc"; 
+// let params = Object.entries( testUrl.split("?")[1].split("&").map(p => p.split("=")) );
+// console.log(params); // Output: { query: "login", page: "2", sort: "asc" }
+
+
+//------------------------------------------------------
 
 // let token = "Bearer eyJhbGciOiJIUzI1NiJ9.secret"; 
 // let masked = token.replace(/(?<=Bearer ).+/, "***REDACTED***");
+// console.log(masked);
